7d69f4f6cd02ea65c863edc0825a4fc886c90079
hieunguyen702 <hieu.nguyen@bys.vn>
2026-07-01 00:43:47 +0700

    Remove .idea from repository

--- src/test/java/config/Init.java
+++ src/test/java/config/Init.java
@@ -1,4 +1,4 @@
-package Initial;
+package config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
--- src/test/java/org/qa/BaiTap1/AssertExample.java
+++ src/test/java/org/qa/BaiTap1/AssertExample.java
@@ -1,10 +1,8 @@
 package org.qa.BaiTap1;
 
-import Initial.Init;
+import config.Init;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
